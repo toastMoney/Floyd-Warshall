b0aef4ad382d9187cfadca965132d4a69ee279e4
t-g-p <tobias.perelstein@gmail.com>
2014-09-30 04:13:19 -0400

    threads in progress

--- wtc_proc.c
+++ wtc_proc.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-//struct graph_struct_{
-//	int vertices;
-//	int processes;
-//	int** matrix;
-//};
-//typedef struct graph_struct_ graph_struct;
 int num_vertices, num_processes;
 
 int** initialize_graph(num_vertices){
@@ -36,6 +31,15 @@ int** initialize_graph(num_vertices){
         printf("\n");
     }
     return ret;
+}
+void print_graph(int** graph){
+    int i,j;
+    for(i=0; i<num_vertices; i++){
+        for(j=0; j<num_vertices; j++){
+            printf("%d",graph[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int** build_graph(){
@@ -79,15 +83,8 @@ int** build_graph(){
         }
     }
 
-    int k,l;
     printf("\n\ngraph after initialization: \n");
-    for(k=0; k<num_vertices; k++){
-        for(l=0; l<num_vertices; l++){
-            printf("%d",graph[k][l]);
-        }
-        printf("\n");
-    }
-
+    print_graph(graph);
 
     if (line){
         free(line);
@@ -95,14 +92,12 @@ int** build_graph(){
     return graph;
 }
 
-void build_trans_closure(){
+void *build_trans_closure(){
 
     int** result_graph;
     result_graph = build_graph();
 
-    int i, j, k ,o, p;
-
-
+    int i, j, k;
     for(k = 0; k < num_vertices; k++){
         for(i = 0; i < num_vertices; i++){
             for(j = 0; j < num_vertices; j++){
@@ -113,18 +108,35 @@ void build_trans_closure(){
     }
 
     printf("\n\ngraph after transitive closure:\n");
-    for(o=0; o<num_vertices; o++){
-        for(p=0; p<num_vertices; p++){
-            printf("%d",result_graph[o][p]);
-        }
-        printf("\n");
-    }
+    print_graph(result_graph);
+    printf("Num vertices %d\n",num_vertices);
+    printf("Num processes: %d\n",num_processes);
+    return NULL;
 }
 
-void main(){
-    //int** graph1;
-    //graph1 = build_graph();
+//void main(){
+//    //int** graph1;
+//    //graph1 = build_graph();
+//    build_graph();
+//    build_trans_closure();
+
+int main(int argc, char **argv) {
     build_graph();
-    build_trans_closure();
+    int i, error;
+    pthread_t thread_pool[num_processes]; // Array of threads
 
+    for (i = 0; i < num_processes; i++) {
+        error = pthread_create(&thread_pool[i], NULL, &build_trans_closure, (int*)i);
+        if (error) {
+            printf("Error creating phtread\n");
+            return error;
+        }
+    }
+
+    for (i = 0; i < num_processes; i++) {
+        pthread_join(thread_pool[i], NULL);
+    }
+
+    return 0;
 }
+
