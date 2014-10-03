fc9cff48d86fe2c8a9d95fc22d6721a27dce197e
t-g-p <tobias.perelstein@gmail.com>
2014-10-02 23:15:16 -0400

    Setting up processes and modifying trans_closure function

--- wtc_proc.c
+++ wtc_proc.c
@@ -11,8 +11,60 @@ int num_vertices, num_processes;
 int shmid;
 int* graph;
 
+struct range_{
+    int start;
+    int end;
+};
+typedef struct range_ range;
+
 void print_graph(void);
 
+range* create_range(){
+    range *temp_range = malloc(sizeof(range));
+    temp_range->start = 0;
+    temp_range->end = 0;
+    position = 0;
+    return temp_range;
+}
+
+range* reset_range(range* arg_range){
+    arg_range->start = 0;
+    arg_range->end = 0;
+    return arg_range;
+}
+
+range* set_range(range* arg_range){
+
+    if(arg_range->end == 0){
+        arg_range->start = 0;
+        arg_range->end = (arg_range->start + vert_per_process - 1);
+        position = arg_range->end;
+
+        printf("check range #1// start: %d\t\tend: %d\n", arg_range->start, arg_range->end);
+        if(mod_count > 0){
+            arg_range->end++;
+            mod_count--;
+            position = arg_range->end;
+        }
+        printf("check range #2// start: %d\t\tend: %d\n", arg_range->start, arg_range->end);
+        return arg_range;
+    }
+
+    else{
+        printf("check range #3// start: %d\t\tend: %d\n", arg_range->start, arg_range->end);
+        arg_range->start = position + 1;
+        arg_range->end = (arg_range->start + vert_per_process - 1);
+        position = arg_range->end;
+        if(mod_count > 0){
+            arg_range->end++;
+            mod_count--;
+            position = arg_range->end;
+        }
+
+        printf("check range #4// start: %d\t\tend: %d\n", arg_range->start, arg_range->end);
+    }
+    return arg_range;
+}
 void initialize_graph(int num_ver){
     int i,index;
     int n = num_ver*num_ver;
@@ -91,27 +143,21 @@ void build_graph(){
     return;
 }
 
-//void *build_trans_closure(){
-//
-//    int** result_graph;
-//    result_graph = build_graph();
-//
-//    int i, j, k;
-//    for(k = 0; k < num_vertices; k++){
-//        for(i = 0; i < num_vertices; i++){
-//            for(j = 0; j < num_vertices; j++){
-//                if(result_graph[i][k] == 1 && result_graph[k][j] == 1)
-//                    result_graph[i][j] = 1;
-//            }
-//        }
-//    }
-//
-//    printf("\n\ngraph after transitive closure:\n");
-//    print_graph(result_graph);
-//    printf("Num vertices %d\n",num_vertices);
-//    printf("Num processes: %d\n",num_processes);
-//    return NULL;
-//}
+void build_trans_closure(int k,int start,int end){
+
+    int i, j;
+    for(i = start; i <= end; i++){
+        for(j = 1; j <= num_vertices; j++){
+            if((graph[(num_vertices*i+k)-num_vertices] = 1) && graph[(num_vertices*k+j)-num_vertices] == 1){
+            graph[(num_vertices*i+j)-num_vertices] = 1;
+            }
+        }
+    }
+    printf("\n\ngraph after transitive closure:\n");
+    print_graph(graph);
+    printf("Num vertices %d\n",num_vertices);
+    printf("Num processes: %d\n",num_processes);
+}
 
 //void main(){
 //    //int** graph1;
@@ -122,7 +168,7 @@ void build_graph(){
 int main(int argc, char **argv) {
     key_t shmkey;
     pid_t pid;
-    int n;
+    int i,j,k,vert_per_process;
     /* initialize a shared variable in shared memory */
     shmkey = ftok ("/dev/null", 5);       /* valid directory name and a number */
     printf ("shmkey for p = %d\n", shmkey);
@@ -133,11 +179,44 @@ int main(int argc, char **argv) {
         //perror ("shmget\n");
         exit (1);
     }
-
     graph = (int*)shmat(shmid, NULL, 0);   /* attach p to shared memory */
     build_graph();
     print_graph();
-    printf ("p=%d is allocated in shared memory.\n\n", *graph);
+
+    if(num_processes)
+        vert_per_process = num_vertices/num_processes;
+
+    if(num_processes == 0)
+        mod_count = 0;
+    else
+        mod_count = num_vertices % num_processes;
+    range *current;
+    current = create_range();
+    for(k = 1; k<=num_vertices; k++){
+        current = reset_range(current);
+        for(i = 0; i<num_processes; i++){
+            //fork
+            if(current){
+                current = set_range(current);
+                printf("start: %d\t\tend: %d\n", current->start, current->end);
+            }
+            else{
+                printf("current not initialized");
+            }
+            printf("current->start: %d\t\tcurrent->end: %d\n\n", current->start, current->end);
+            pid = fork();
+            if(pid == 0){
+                build_trans_closure(k,current->start,current->end);
+            }
+            else if (pid<0) {
+                printf("failed to fork!\n");
+            }
+            else{
+                for(j=0; j<num_processes; j++){
+                    wait(0);
+                }
+            }
+        }
+    }
     return 0;
 }
-
